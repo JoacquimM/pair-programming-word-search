3d52a53afb9e22ec49ee581eab6836d44ea93b8d
Joacquim Mukadi <joacquimm@gmail.com>
2021-03-11 14:04:16 +0000

    test_wordsearch.js

--- test/test_wordsearch.js
+++ test/test_wordsearch.js
@@ -1,3 +1,43 @@
+// const chai = require('chai');
+// const assert = chai.assert;
+
+// const wordSearch = require('../wordsearch.js')
+
+// describe("#wordSearch()", function() {
+//   it("should return false if the word is not present", function() {
+//     const result = wordSearch([
+//       ['A', 'W', 'C', 'F', 'Q', 'U', 'A', 'L'],
+//       ['S', 'E', 'I', 'N', 'F', 'E', 'L', 'D'],
+//       ['Y', 'F', 'C', 'F', 'Q', 'U', 'A', 'L'],
+//       ['H', 'M', 'J', 'T', 'E', 'V', 'R', 'G'],
+//       ['W', 'H', 'C', 'S', 'Y', 'E', 'R', 'L'],
+//       ['B', 'F', 'R', 'E', 'N', 'E', 'Y', 'B'],
+//       ['U', 'B', 'T', 'W', 'A', 'P', 'A', 'I'],
+//       ['O', 'D', 'C', 'A', 'K', 'U', 'A', 'S'],
+//       ['E', 'Z', 'K', 'F', 'Q', 'U', 'A', 'L'],
+//     ], 'FRANK')
+
+//     assert.isFalse(result);
+//   });
+
+//   it("should return true if the word is present", function() {
+//     const result = wordSearch([
+//       ['A', 'W', 'C', 'F', 'Q', 'U', 'A', 'L'],
+//       ['S', 'E', 'I', 'N', 'F', 'E', 'L', 'D'],
+//       ['Y', 'F', 'C', 'F', 'Q', 'U', 'A', 'L'],
+//       ['H', 'M', 'J', 'T', 'E', 'V', 'R', 'G'],
+//       ['W', 'H', 'C', 'S', 'Y', 'E', 'R', 'L'],
+//       ['B', 'F', 'R', 'E', 'N', 'E', 'Y', 'B'],
+//       ['U', 'B', 'T', 'W', 'A', 'P', 'A', 'I'],
+//       ['O', 'D', 'C', 'A', 'K', 'U', 'A', 'S'],
+//       ['E', 'Z', 'K', 'F', 'Q', 'U', 'A', 'L'],
+//     ], 'SEINFELD')
+
+//     assert.isTrue(result);
+//   });
+// });
+//test_word.js
+
 const chai = require('chai');
 const assert = chai.assert;
 
@@ -32,7 +72,22 @@ describe("#wordSearch()", function() {
       ['O', 'D', 'C', 'A', 'K', 'U', 'A', 'S'],
       ['E', 'Z', 'K', 'F', 'Q', 'U', 'A', 'L'],
     ], 'SEINFELD')
-
+    
     assert.isTrue(result);
+
   });
+  it('it should return true when the word exists vertically', function() {
+    const result = wordSearch([
+      ['A', 'W', 'C', 'F', 'Q', 'U', 'A', 'L'],
+      ['S', 'O', 'I', 'N', 'F', 'E', 'L', 'D'],
+      ['Y', 'R', 'C', 'F', 'Q', 'U', 'A', 'L'],
+      ['H', 'D', 'J', 'T', 'E', 'V', 'R', 'G'],
+      ['W', 'H', 'C', 'S', 'Y', 'E', 'R', 'L'],
+      ['B', 'F', 'R', 'E', 'N', 'E', 'Y', 'B'],
+      ['U', 'B', 'T', 'W', 'A', 'P', 'A', 'I'],
+      ['O', 'D', 'C', 'A', 'K', 'U', 'A', 'S'],
+      ['E', 'Z', 'K', 'F', 'Q', 'U', 'A', 'L'],
+    ], 'WORD')
+    assert.isTrue(result);
+  })
 });
