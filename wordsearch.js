8bb8a03496b3c8134d797eddf333e2e0f591fdc8
Joacquim Mukadi <joacquimm@gmail.com>
2021-03-11 14:02:31 +0000

    wordsearch.js file

--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,49 @@
-const wordSearch = (letters, word) => { 
-    const horizontalJoin = letters.map(ls => ls.join(''))
-    for (l of horizontalJoin) {
-        if (l.includes(word)) return true
-    }
-}
+// const wordSearch = (letters, word) => { 
+//     const horizontalJoin = letters.map(ls => ls.join(''))
+//     for (l of horizontalJoin) {
+//         if (l.includes(word)) return true
+//     }
+// }
 
-module.exports = wordSearch
+// module.exports = wordSearch
+
+
+//wordsearch.js
+
+const transpose = function(matrix) {
+    let newMatrix = [];
+    for (let i = 0; i < matrix.length; i ++){
+      let newArr = matrix[i]
+  
+          for (let j = 0; j < newArr.length; j ++){
+            let newArrJ = newArr[j];
+            if (!newMatrix[j]){
+              newMatrix[j] = [];
+            }
+            newMatrix[j][i] = newArrJ
+          }
+    }
+  return newMatrix
+  };
+  
+  const wordSearch = (letters, word) => { 
+      const horizontalJoin = letters.map(ls => ls.join(''))
+      const verticalJoin = transpose(letters).map(ls => ls.join(''))
+      console.log(verticalJoin)
+      
+      for (let l of horizontalJoin) {
+        if (l.includes(word)) {
+          return true
+        } 
+        console.log(l)
+      }
+      for(let j of verticalJoin) {
+        if (j.includes(word)) {
+          return true
+        }
+      }   
+    return false
+  }
+  
+  
+  module.exports = wordSearch
